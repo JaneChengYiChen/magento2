e0c1244cf800edbffd773261d395e8291a2031ef
Jane Cheng <freedomerest@gmail.com>
2022-01-20 13:49:11 +0800

    feat:add registration

---
+++ app/code/assignment/HelloWorld/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+
+* @category Assignment
+* @package Assignment_HelloWorld
+*
+*/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+\Magento\Framework\Component\ComponentRegistrar::MODULE,
+'Assignment_HelloWorld',
+__DIR__
+);
