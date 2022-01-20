75c2060d82c0e4cef939c7a3a265a006c35e028b
Jane Cheng <freedomerest@gmail.com>
2022-01-20 14:05:26 +0800

    feat: create block

---
+++ app/code/assignment/HelloWorld/Block/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Simple Hello World Module
+*
+* @category Assignment
+* @package Assignment_HelloWorld
+*
+*/
+namespace Assignment\HelloWorld\Block;
+class HelloWorld extends \Magento\Framework\View\Element\Template
+{
+    public function _prepareLayout() {
+        parent::_prepareLayout();
+        $this->pageConfig->getTitle()->set(__('First Hello World Module'));
+        return $this;
+    }
+}
