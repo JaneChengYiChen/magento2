0ae28e1f70dfe9b247a7ef8a34b1d459a48e69a7
Jane Cheng <freedomerest@gmail.com>
2022-01-20 17:55:51 +0800

    blank block

--- app/code/assignment/HelloWorld/Block/Index.php
+++ app/code/assignment/HelloWorld/Block/Index.php
@@ -1,17 +1,10 @@
 <?php
-/**
-* Simple Hello World Module
-*
-* @category Assignment
-* @package Assignment_HelloWorld
-*
-*/
 namespace Assignment\HelloWorld\Block;
 class Index extends \Magento\Framework\View\Element\Template
 {
-    public function _prepareLayout() {
-        parent::_prepareLayout();
-        $this->pageConfig->getTitle()->set(__('First Hello World Module'));
-        return $this;
-    }
+    // public function _prepareLayout() {
+    //     parent::_prepareLayout();
+    //     $this->pageConfig->getTitle()->set(__('First Hello World Module'));
+    //     return $this;
+    // }
 }
