040105ec61af3518eb82f222a0cb2859cb699b6a
Jane Cheng <freedomerest@gmail.com>
2022-01-21 11:55:43 +0800

    feat: add shop

---
+++ app/code/Shop/ItemPage/Controller/Page/Currency.php
@@ -0,0 +1,40 @@
+<?php
+namespace Shop\ItemPage\Controller\Page;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Currency extends Action
+{
+    /**
+     * @var JsonFactory
+     */
+    protected $resultJsonFactory;
+
+    /**
+     * @param Context $context
+     * @param JsonFactory $resultJsonFactory
+     */
+    public function __construct(
+        Context $context,
+        JsonFactory $resultJsonFactory
+    ) {
+        parent::__construct($context);
+        $this->resultJsonFactory = $resultJsonFactory;
+    }
+
+    /**
+     * View  page action
+     *
+     * @return ResultInterface
+     */
+    public function execute()
+    {
+        $result = $this->resultJsonFactory->create();
+        $data = ['message' => 'This is currency page!'];
+
+        return $result->setData($data);
+    }
+}
