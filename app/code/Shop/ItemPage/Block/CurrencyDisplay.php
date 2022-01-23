78bbb92c9343c972438d06463e9c2e02d832fff1
Jane Cheng <freedomerest@gmail.com>
2022-01-23 23:18:35 +0800

    fix: try to use jsonfactory

--- app/code/Shop/ItemPage/Block/CurrencyDisplay.php
+++ app/code/Shop/ItemPage/Block/CurrencyDisplay.php
@@ -3,12 +3,14 @@ namespace Shop\ItemPage\Block;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\Controller\Result\JsonFactory;
 
 class CurrencyDisplay extends Template
 {
     public function __construct(Context $context)
 	{
 		parent::__construct($context);
+        $this->resultJsonFactory = new JsonFactory;
 	}
 
     public function getContent()
@@ -18,9 +20,11 @@ class CurrencyDisplay extends Template
 
     private function curlApi()
     {
+        $result = $this->resultJsonFactory->create();
         $url ='https://tw.rter.info/capi.php';
         $url_data = file_get_contents($url);
+        $currency=json_decode($url_data); 
 
-        return json_decode($url_data);
+        return $result->setData($currency);
     }
 }
