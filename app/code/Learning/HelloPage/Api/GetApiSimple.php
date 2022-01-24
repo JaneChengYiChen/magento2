813f6fe84f9b1321ee7f900d24f828257005de37
Jane Cheng <freedomerest@gmail.com>
2022-01-24 12:45:39 +0800

    fix:get content

--- app/code/Learning/HelloPage/Api/GetApiSimple.php
+++ app/code/Learning/HelloPage/Api/GetApiSimple.php
@@ -24,6 +24,6 @@ class GetApiSimple
         ]);
 
         $response = $client->request('GET', '/repos/magento/magento2');
-        return $response;
+        return $response->getContents();
     }
 }
